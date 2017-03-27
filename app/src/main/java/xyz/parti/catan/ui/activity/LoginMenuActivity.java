c3fcd0b9033174fa9f7b916a585505b257c0c8b9
dali <dalikim@parti.xyz>
2017-03-27 14:44:32 +0900

    로그인을 위해 페이스북 정보를 얻어옵니다

--- app/src/main/java/xyz/parti/catan/ui/activity/LoginMenuActivity.java
+++ app/src/main/java/xyz/parti/catan/ui/activity/LoginMenuActivity.java
@@ -4,21 +4,37 @@ import android.content.Intent;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+
+import java.util.Arrays;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import xyz.parti.catan.Constants;
 import xyz.parti.catan.R;
 
 public class LoginMenuActivity extends AppCompatActivity {
     @BindView(R.id.loginByEmailButton)
     View loginByEmailButton;
+    @BindView(R.id.loginByFacebookButton)
+    View loginByFacebookButton;
     @BindView(R.id.signUpLink)
     View signUpLink;
+
+    private CallbackManager callbackManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        callbackManager = initFacebook();
         setContentView(R.layout.activity_login_menu);
         ButterKnife.bind(LoginMenuActivity.this);
 
@@ -29,6 +45,12 @@ public class LoginMenuActivity extends AppCompatActivity {
                 LoginMenuActivity.this.startActivity(i);
             }
         });
+        loginByFacebookButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                LoginManager.getInstance().logInWithReadPermissions(LoginMenuActivity.this, Arrays.asList("email", "user_friends"));
+            }
+        });
 
         signUpLink.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -37,5 +59,32 @@ public class LoginMenuActivity extends AppCompatActivity {
                 startActivity(i);
             }
         });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+    }
+
+    private CallbackManager initFacebook() {
+        CallbackManager callbackManager = CallbackManager.Factory.create();
+        LoginManager.getInstance().registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                Log.d(Constants.TAG, "페이스북 로그인 성공");
+            }
+
+            @Override
+            public void onCancel() {
+
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+
+            }
+        });
+        return callbackManager;
     }
 }
